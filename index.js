c475d565ed8b42d46ad36e8f996610dba323e673
Ange1D <47231898+Ange1D@users.noreply.github.com>
2022-06-11 21:25:27 -0500

    Estados y Propiedades

--- index.js
+++ index.js
@@ -7,24 +7,27 @@ function changeState(){
 
     
     if (navMenu.classList.contains('nav-menu-open')) {
-        menuOpen.setAttribute("aria-expanded", "true");
+        menuOpen.setAttribute("aria-expanded","true");
         navContainer.removeAttribute("hidden");
     } 
 
     if(!menuOpen.classList.contains('menu-open')){
-        menuOpen.setAttribute("aria-expanded", "false");
+        menuOpen.setAttribute("aria-expanded","false");
         navContainer.setAttribute("hidden", "true");
     }
 };
 
 document.addEventListener('DOMContentLoaded', (e) => {
+
     let ratio = window.devicePixelRatio || 1;
     let w = screen.width * ratio;
     const navContainer = document.querySelector('#nav-container');
 
     if(w < 960) {
+
         navContainer.setAttribute("aria-hidden", "true");
     }else{
+        
         navContainer.removeAttribute('aria-hidden')
         navContainer.removeAttribute("hidden");
     }
